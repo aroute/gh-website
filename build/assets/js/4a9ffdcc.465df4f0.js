659a6db936d4f45e89385c4784e39db8b46bd878
Arif <arifroute@outlook.com>
2023-08-01 15:41:59 -0500

    Second

---
+++ build/assets/js/4a9ffdcc.465df4f0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgh_website=self.webpackChunkgh_website||[]).push([[882],{6102:e=>{e.exports=JSON.parse('{"title":"Workshop","description":"MAS Workshop\'s getting started guide.","slug":"/category/workshop","permalink":"/category/workshop","navigation":{"next":{"title":"OCP MAS workshop","permalink":"/"}}}')}}]);
--- docs/intro.md
+++
@@ -1,6 +0,0 @@
----
-slug: /
-position: 1
----
-
-# Hello World
